f8246750161f4a1c1e97a4b7b10c88debe8b1219
karl <yiiso@qq.com>
2025-08-26 09:27:19 +0800

    增加 tiktok下载图片

--- app/Service/TiktokService.php
+++ app/Service/TiktokService.php
@@ -7,10 +7,44 @@ use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 class TiktokService
 {
+    public function parseImageFromAPI($videoUrl): array
+    {
+        $demo = 'https://www.tiktok.com/@alejocroes/photo/7525722589738650913?is_from_webapp=1&sender_device=pc';
+
+
+        if ( $videoUrl) {
+            $segments = collect(explode('/', parse_url($videoUrl, PHP_URL_PATH)));
+
+            $type = $segments->get(2); // photo 或 video
+            $id   = $segments->get(3); // 7525722589738650913
+
+            $realUrl = env('PARSER_TIKTOK_URL').'/api/tiktok/web/fetch_one_video?itemId='.$id;
+
+            $ch = curl_init();
+            curl_setopt_array($ch, [
+                CURLOPT_URL => $realUrl,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                    'Accept: application/json',
+                    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+                ],
+                CURLOPT_TIMEOUT => 30,
+            ]);
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($response, true);
+            return $this->formatResponse($data['data'] ?? []);
+        }
+        throw new \Exception('error: ' . ($data['message'] ?? 'unknown'));
+
+    }
     public function parseVideoFromAPI($videoUrl)
     {
         $demo = 'https://www.tiktok.com/@alejocroes/video/7525722589738650913?is_from_webapp=1&sender_device=pc';
-
+        if (preg_match('#tiktok\.com/.+/photo/#', $videoUrl)) {
+            return $this->parseImageFromAPI($videoUrl);
+        }
 
         if ( $videoUrl) {
 
@@ -36,6 +70,19 @@ class TiktokService
 
     }
 
+    private function getImages(array $data)
+    {
+        $images = [];
+        if (isset($data['itemInfo']['itemStruct']['imagePost']['images'])){
+            foreach ($data['itemInfo']['itemStruct']['imagePost']['images'] as $img) {
+                if (isset($img['imageURL']['urlList'][0])){
+                    $images[]['url'] = $img['imageURL']['urlList'][0];
+                }
+            }
+        }
+        return $images;
+    }
+
     private function formatResponse(array $data): array
     {
         $formatted = [
@@ -44,7 +91,8 @@ class TiktokService
             'duration' => $data['duration'] ?? __('messages.unknown_duration'),
             'author' => isset($data['author']) ? ($data['author']['nickname'] ??  '') : __('messages.unknown_author'),
             'quality_options' => [],
-            'audio_options' => []
+            'audio_options' => [],
+            'images' => $this->getImages($data),
         ];
 
         // 处理视频下载链接 - 尝试多种可能的字段名
@@ -53,18 +101,18 @@ class TiktokService
             $formatted['quality_options'][] = [
                 'quality' => __('messages.original_quality'),
                 'format' => 'mp4',
-                'size' => number_format($data['video']['play_addr']['data_size']/1024/1024, 2) ?? 0,
+                'size' =>   0,
                 'download_url' => $videoUrl
             ];
         }
 
         // 处理音频下载链接
-        if (isset($data['music']['play_url']['uri'])) {
+        if (isset($data['music']['play_url']['uri']) || isset($data['itemInfo']['itemStruct']['music']['playUrl'])) {
             $formatted['audio_options'][] = [
                 'quality' => __('messages.original_audio_quality'),
                 'format' => 'mp3',
                 'size' => $data['audio_size'] ?? 0,
-                'download_url' => $data['music']['play_url']['uri']
+                'download_url' => $data['music']['play_url']['uri'] ?? ($data['itemInfo']['itemStruct']['music']['playUrl'])
             ];
         }
         return $formatted;
